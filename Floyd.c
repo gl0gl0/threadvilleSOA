f95272910dc1ccc2ee040253a9230be83a6aced6
Alejandro <abaltodano@ubuntu-laptop.(none)>
2014-05-24 21:56:47 -0600

    Agregando un Floyd y el archivo de rutas

---
+++ Floyd.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#ifndef min
+	#define min( a, b ) ( ((a) < (b)) ? (a) : (b) )
+#endif
+
+#define N 714
+
+int Threadville[N][N];
+
+
+
+void llenarMatriz(){
+	int i, j;
+	for (i=0; i<N; i++){
+		for (j=0; j<N; j++){
+			Threadville[i][j] = 0;
+		}
+	}
+	int s14;
+	s14 = 0;
+	for (i=0; i<N; i++){
+		for (j=0; j<N; j++){
+			if (s14 == 14){
+				s14 = 0;
+				Threadville[i][j] = j + 
+			}
+			Threadville[i][j] = j + 1;
+		}
+	}
+}
+
+	A	B	C
+A	0	C	1
+B		0	1
+C	1		0
+
+void Floyd(){
+	int i, j, k;
+	for (k=0; k<N; k++){
+		for (i=0; i<N; i++){
+			for (j=0; j<N; j++){
+				Threadville[i][j] = min(Threadville[i][j], Threadville[i][k] + Threadville[k][j]);
+			}
+		}
+	}
+}
+
+int main(int argc, char* argv){
+	Floyd();
+	return 0;
+}
